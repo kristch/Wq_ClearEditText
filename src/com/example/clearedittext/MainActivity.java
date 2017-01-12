4a7e59fe8e9fac965133f93aaeb02c33d72ee4ab
kristch <wwq2862108920@163.com>
2017-01-12 14:03:12 +0800

    可以清楚的EditText

---
+++ src/com/example/clearedittext/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.clearedittext;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	private Toast mToast;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_main);
+		
+		final ClearEditText username = (ClearEditText) findViewById(R.id.username);
+		final ClearEditText password = (ClearEditText) findViewById(R.id.password);
+		
+		((Button)findViewById(R.id.login)).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if(TextUtils.isEmpty(username.getText())){
+					//设置晃动
+					username.setShakeAnimation();
+					//设置提示
+					showToast("用户名不能为空");
+					return;
+				}
+				
+				if(TextUtils.isEmpty(password.getText())){
+					password.setShakeAnimation();
+					showToast("密码不能为空");
+					return;
+				}
+			}
+		});
+	}
+	
+	/**
+	 * 显示Toast消息
+	 * @param msg
+	 */
+	private void showToast(String msg){
+		if(mToast == null){
+			mToast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+		}else{
+			mToast.setText(msg);
+		}
+		mToast.show();
+	}
+
+
+}
